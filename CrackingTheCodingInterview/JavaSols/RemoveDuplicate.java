9098c041cd9c01d5584a76414addcb8e17836412
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-07-27 21:20:34 +0300

    Solved 2.1 in java

---
+++ CrackingTheCodingInterview/JavaSols/RemoveDuplicate.java
@@ -0,0 +1,66 @@
+package CrackingTheCodingInterview.JavaSols;
+
+public class RemoveDuplicate {
+
+    public static Node removeDuplicate(Node head) {
+        Node current = head;
+        while (current != null) {
+            Node fastNode = current;
+            while (fastNode.getNext() != null) {
+                if (fastNode.getNext().getData() == current.getData()) {
+                    fastNode.setNext(fastNode.getNext().getNext());
+                } else {
+                    fastNode = fastNode.getNext();
+                }
+            }
+            current = current.getNext();
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node n1 = new Node('1');
+        Node n2 = new Node('1');
+        Node n3 = new Node('1');
+        Node n4 = new Node('1');
+        Node n5 = new Node('2');
+
+        n1.setNext(n2);
+        n2.setNext(n3);
+        n3.setNext(n4);
+        n4.setNext(n5);
+
+        n1 = removeDuplicate(n1);
+        while (n1 != null) {
+            System.out.print(n1.getData() + " ");
+            n1 = n1.getNext();
+        }
+    }
+}
+
+class Node {
+    private char data;
+    private Node next;
+
+    public Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public void setData(char data) {
+        this.data = data;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public char getData() {
+        return this.data;
+    }
+
+    public Node getNext() {
+        return this.next;
+    }
+}
