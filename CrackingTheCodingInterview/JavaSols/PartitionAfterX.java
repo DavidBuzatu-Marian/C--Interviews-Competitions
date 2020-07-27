a5db015eaefc15323e3508c39603c5efc04e61d3
davidbuzatu-marian <davidm.buzatu@gmail.com>
2020-07-27 22:28:52 +0300

    Solved 2.4 in java

---
+++ CrackingTheCodingInterview/JavaSols/PartitionAfterX.java
@@ -0,0 +1,41 @@
+package CrackingTheCodingInterview.JavaSols;
+
+public class PartitionAfterX {
+
+    public static Node splitAfterPivot(Node node, char x) {
+        Node nextNode, tail = node, newHead = node;
+        while (node != null) {
+            nextNode = node.getNext();
+            if (node.getData() > x) {
+                tail.setNext(node);
+                tail = node;
+            } else {
+                node.setNext(newHead);
+                newHead = node;
+            }
+            node = nextNode;
+        }
+        tail.setNext(null);
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        Node n1 = new Node('4');
+        Node n2 = new Node('3');
+        Node n3 = new Node('1');
+        Node n4 = new Node('7');
+        Node n5 = new Node('5');
+        Node n6 = new Node('1');
+        n1.setNext(n2);
+        n2.setNext(n3);
+        n3.setNext(n4);
+        n4.setNext(n5);
+        n5.setNext(n6);
+
+        n1 = splitAfterPivot(n1, '3');
+        while (n1 != null) {
+            System.out.println(n1.getData());
+            n1 = n1.getNext();
+        }
+    }
+}
